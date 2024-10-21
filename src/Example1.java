c2457ac0db9110f1c9a369d8c57ea7d613923eec
Maryam Ayomide Banjo <D00272896@student.dkit.ie>
2024-10-21 13:28:08 +0100

    Learning about object and classes

---
+++ src/Example1.java
@@ -0,0 +1,17 @@
+public class Example1 {
+    String name;
+    int var1;
+//    instance variable
+
+//    You can have methods in your  class
+    void methodName( String parameters){
+
+    }
+   void attack(){
+       System.out.println(name +" attack");
+   }
+   Example1(){
+       int level = 1;
+   }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Example1 e = new Example1();
+
+/*
+    Creating a new object
+        e.name = "pikachu";
+        e.var1 = 10;
+
+        System.out.println(e.name + " " + e.var1);
+
+        Example1 e2 = new Example1();
+        e2.name =  "Eviee";
+
+        e2.attack();
+*/
+    }
+}
